732d277010ce61f542afdab30e666bd8e099d9db
Cosumicu <chestercristobalv2@gmail.com>
2025-12-11 15:08:44 +0800

    update logging

--- booking_site/settings.py
+++ booking_site/settings.py
@@ -238,36 +238,10 @@ logger = logging.getLogger(__name__)
 
 LOG_LEVEL = "INFO"
 
-logging.config.dictConfig({
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "console": {
-            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-        },
-        "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
-        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "console",
-        },
-        "file":{
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "formatter":"file",
-            "filename":"logs/booking_site.log",
-        },
-        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-    },
-    "loggers":{
-        "":{"level": "INFO", "handlers": ["console", "file"],
-            "propagate": False},
-            "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
-            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
-    }
-})
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)-8s %(name)s %(message)s"
+)
 
 """
 So basically, what s happening here is that I used celery email backends to handle emails
