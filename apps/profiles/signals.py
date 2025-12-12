d81dab770f4782026746cad980c70e90b7080926
Cosumicu <chestercristobalv2@gmail.com>
2025-12-12 12:28:59 +0800

    fix minor issues

--- apps/profiles/signals.py
+++ apps/profiles/signals.py
@@ -1,17 +1,19 @@
 import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from booking_site.settings import AUTH_USER_MODEL
 from apps.profiles.models import Profile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 logger = logging.getLogger(__name__)
 
-@receiver(post_save, sender=AUTH_USER_MODEL)
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=AUTH_USER_MODEL)
+@receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
     logger.info(f"{instance}'s Profile Created.")
