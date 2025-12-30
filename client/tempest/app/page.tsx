41dcf6531ce10ed53cec734cf6eaf1eb5f47c717
Cosumicu <chestercristobalv2@gmail.com>
2025-12-30 18:36:18 +0800

    added note

--- client/tempest/app/page.tsx
+++ client/tempest/app/page.tsx
@@ -1,8 +1,30 @@
 import Properties from "./components/properties/Properties";
+import { Alert } from "antd";
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
   return (
     <>
+      <Alert
+        banner
+        message={
+          <Marquee gradient={false}>
+            <div style={{ display: "inline" }}>
+              Note: SMTP service free trial expired—some features won’t work.
+              Ready-to-use accounts available on my{" "}
+              <a
+                href="https://github.com/Cosumicu/mita-site"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub repo
+              </a>
+              .
+            </div>
+          </Marquee>
+        }
+      />
+
       <Properties />
     </>
   );
