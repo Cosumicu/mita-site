4fc56635927356fbdcb3fe2534f62f5e4e74bb86
Cosumicu <chestercristobalv2@gmail.com>
2026-01-02 16:13:34 +0800

    update static dashboard

--- client/tempest/app/host/dashboard/page.tsx
+++ client/tempest/app/host/dashboard/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useAppDispatch, useAppSelector } from "@/app/lib/hooks";
+import { Segmented } from "antd";
+import { useState } from "react";
 import {
   AreaChart,
   Area,
@@ -10,6 +12,8 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
+
+type Range = "week" | "month" | "year";
 
 const bookingsData = [
   { date: "Jan", count: 12 },
@@ -28,11 +32,48 @@ const revenueData = [
 const DashboardPage = () => {
   const dispatch = useAppDispatch();
 
+  const [range, setRange] = useState<"week" | "month" | "year">("month");
+
   return (
-    <div className="m-4">
-      <div className="m-4 space-y-6">
+    <div className="m-8">
+      <div className="space-y-8">
+        <div className="flex gap-2 items-center">
+          <p className="font-semibold sm:text-3xl">Dashboard</p>
+          <p className="text-gray-500">Save Data</p>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="gray"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-download"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+              <path d="M7 11l5 5l5 -5" />
+              <path d="M12 4l0 12" />
+            </svg>
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <Segmented<Range>
+            options={[
+              { label: "This Week", value: "week" },
+              { label: "This Month", value: "month" },
+              { label: "This Year", value: "year" },
+            ]}
+            value={range}
+            onChange={(value) => setRange(value)}
+          />
+        </div>
+
         {/* Stats cards */}
-        <div className="flex justify-between gap-4 p-4 border border-gray-200 rounded-lg">
+        <div className="flex justify-between gap-4 p-6 border border-gray-200 rounded-lg">
           <div className="flex-1 bg-white rounded-xl p-4 border-l-4 border-l-primary shadow-lg">
             <p>Total Monies</p>
             <p>Admin</p>
@@ -40,22 +81,18 @@ const DashboardPage = () => {
             <p>Admin</p>
           </div>
           <div className="flex-1 bg-white rounded-xl p-4 border-l-4 border-l-primary shadow-lg">
-            <p>Upcoming Reservations</p>
-            <p>Admin</p>
-            <p>Admin</p>
-            <p>Admin</p>
+            <p>Occupancy Rate</p>
+            <p className="text-3xl font-semibold my-2">67%</p>
           </div>
           <div className="flex-1 bg-white rounded-xl p-4 border-l-4 border-l-primary shadow-lg">
-            <p>Pending Requests</p>
-            <p>Admin</p>
-            <p>Admin</p>
-            <p>Admin</p>
+            <p>Total Income</p>
+            <p className="text-3xl font-semibold my-2">PHP 12,300</p>
+            <p className="font-semibold text-green-600">+12%</p>
           </div>
           <div className="flex-1 bg-white rounded-xl p-4 border-l-4 border-l-primary shadow-lg">
-            <p>Ave. Nightly Rate</p>
-            <p>Admin</p>
-            <p>Admin</p>
-            <p>Admin</p>
+            <p>{"ADR (PHP)"}</p>
+            <p className="text-3xl font-semibold my-2">PHP 1,300</p>
+            <p className="font-semibold text-red-600">-5%</p>
           </div>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
